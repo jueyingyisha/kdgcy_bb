a9371e45811ab4a04348f0f3fbee91bbc7501d71
陈永福 <943627736@qq.com>
2021-11-18 15:16:46 +0800

    提交

---
+++ src/main/java/com/keda/uilt/Text.java
@@ -0,0 +1,8 @@
+package com.keda.uilt;
+
+public class Text {
+
+    public void text(){
+        System.out.println("你是傻逼");
+    }
+}
---
+++ src/main/java/com/keda/uilt/Texture.java
@@ -0,0 +1,7 @@
+package com.keda.uilt;
+
+public class Texture {
+    public void text2(){
+        System.out.println("你还是大傻逼");
+    }
+}
